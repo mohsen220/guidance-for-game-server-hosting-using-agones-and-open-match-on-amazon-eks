4738342d5f4c305c0b9b36b350a64160007cd9b2
mohsenlh <mohsenlh@amazon.com>
2025-05-21 14:47:52 -0400

    test

--- terraform/extra-cluster/versions.tf
+++ terraform/extra-cluster/versions.tf
@@ -2,4 +2,15 @@
 ## SPDX-License-Identifier: MIT-0
 terraform {
   required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.8.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20.0"
+    }
+  }
 }
--- terraform/intra-cluster/versions.tf
+++ terraform/intra-cluster/versions.tf
@@ -8,5 +8,17 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.8.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
   }
 }
